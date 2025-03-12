85d3d8a3a92cf04ea75880a711ba804bdbb1180a
Shina-Kelani Taiwo <kelanitaiwo34@gmail.com>
2025-03-12 05:42:24 +0100

    update url

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 export default function Home() {
 	useEffect(() => {
-		window.location.href = "https://form.jotform.com/250692869975075";
+		window.location.href = "https://paystack.com/pay/campteambonding";
 	}, []);
 
 	return null;
